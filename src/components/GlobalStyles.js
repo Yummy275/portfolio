27d742913f35cfad0b81048f9e4283a4830c5172
Yummy275 <htgonzalez94@yahoo.com>
2022-07-03 14:26:56 -0700

    initial commit

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+import { colors } from '../util/colors';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      font-family: 'Lato', sans-serif;
+      color: ${colors.white};
+  }
+  
+  html {
+    background-color: ${colors.black};
+  }
+`;
---
+++ src/util/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    white: '#FFFCF9',
+    black: '#000000',
+    brown: '#A57F60',
+    lightBlue: '#B1EDE8',
+};
